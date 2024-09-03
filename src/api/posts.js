3739882b031ae19cac5e8acb658960303546e060
Aman <amansangeeth457@gmail.com>
2024-09-03 10:21:44 +0530

    Update fetch URL to use GitHub Pages

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:3000'
+    /* baseURL: 'http://localhost:3000' */
+    baseURL: 'https://github.com/tomcat170701/deploy-blog/blob/main/src/data/db.json'
 });
