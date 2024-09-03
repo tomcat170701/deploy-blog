5372afb20c5984df96a3812960f33db36b8ac7bd
Aman <amansangeeth457@gmail.com>
2024-09-03 10:29:53 +0530

    revert changes

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    /* baseURL: 'http://localhost:3000' */
-    baseURL: 'https://github.com/tomcat170701/deploy-blog/blob/main/src/data/db.json'
+    baseURL: 'http://localhost:3000'
 });
